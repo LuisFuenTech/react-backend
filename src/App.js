3f6f53925119dd48712f9359702dd92ab1e13214
Fuentech <luisfuau10@gmail.com>
2018-12-26 21:45:14 -0500

    Using Toastify from react

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import axios from "axios";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import http from "./services/httpService";
+import config from "./config.json";
 import "./App.css";
-
-const apiUrl = "https://jsonplaceholder.typicode.com/posts";
 
 class App extends Component {
   state = {
@@ -10,13 +11,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiUrl);
+    const { data: posts } = await http.get(config.apiUrl);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiUrl, obj);
+    const { data: post } = await http.post(config.apiUrl, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -24,7 +25,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "Updated";
-    await axios.put(`${apiUrl}/${post.id}`, post);
+    await http.put(`${config.apiUrl}/${post.id}`, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -33,14 +34,24 @@ class App extends Component {
   };
 
   handleDelete = async post => {
-    await axios.delete(`${apiUrl}/${post.id}`);
+    const originalPosts = this.state.posts;
+
     const posts = this.state.posts.filter(p => p.id !== post.id);
     this.setState({ posts });
+    try {
+      await http.delete(`${config.apiUrl}/${post.id}`);
+    } catch (ex) {
+      if (ex.responde && ex.responde === 404)
+        alert("This post has already benn deleted.");
+
+      this.setState({ posts: originalPosts });
+    }
   };
 
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <button className="btn btn-primary" onClick={this.handleAdd}>
           Add
         </button>
